f1cee5648b5d9a7d0b3498d6a010cce29e758968
oruccakir <oruccakir2525@gmail.com>
2023-10-21 13:21:02 +0300

    array copying studied

--- arrays.c
+++ arrays.c
@@ -1,5 +1,36 @@
 #include<stdio.h>
+#include<stdlib.h> 
+  
+# define SIZE 5 
+  
+// A wrapper for array to make sure that array 
+// is passed by value. 
+struct ArrayWrapper 
+{ 
+    int arr[SIZE]; 
+};
 
+// An array is passed by value wrapped in temp 
+void modify(struct ArrayWrapper temp) 
+{ 
+    int *ptr = temp.arr; 
+    int i; 
+  
+    // Display array contents 
+    printf("In 'modify()', before modification\n"); 
+    for (i = 0; i < SIZE; ++i) 
+        printf("%d ", ptr[i]); 
+  
+    printf("\n"); 
+  
+    // Modify the array 
+    for (i = 0; i < SIZE; ++i) 
+        ptr[i] = 100; // OR *(ptr + i) 
+  
+    printf("\nIn 'modify()', after modification\n"); 
+    for (i = 0; i < SIZE; ++i) 
+        printf("%d ", ptr[i]); // OR *(ptr + i) 
+} 
 
 /*
     A whole array cannot be passed as an argument to a function in C++. 
@@ -26,6 +57,17 @@ void print_with_pointer(int *arr,int n){
 
 }
 
+/*
+    In C, array name represents address and when we pass an array,
+     we actually pass address and the parameter receiving function always accepts them as pointers (even if we use [],
+      refer this for details).
+
+How to pass array by value, i.e., how to make sure that we have a new copy of array when we pass it to function?
+This can be done by wrapping the array in a structure and creating a variable of type of that structure and assigning values to that array.
+ After that, passing the variable to some other function and modifying it as per requirements. Note that array members are copied when passed as parameter,
+  but dynamic arrays are not. So this solution works only for non-dynamic arrays (created without new or malloc).
+*/
+
 int main(){
 
     int arr[] = {1,4,7,5,6,25,14,5,5,5,5,5,1,1};
@@ -33,6 +75,20 @@ int main(){
     int n = sizeof(arr)/sizeof(arr[0]);
 
     print_with_pointer(arr,n);
+
+    int i; 
+    struct ArrayWrapper obj; 
+    for (i=0; i<SIZE; i++) 
+        obj.arr[i] = 10; 
+  
+    modify(obj); 
+  
+    // Display array contents 
+    printf("\n\nIn 'Main', after calling modify() \n"); 
+    for (i = 0; i < SIZE; ++i) 
+        printf("%d ", obj.arr[i]); // Not changed 
+  
+    printf("\n"); 
 
     return 0;
 }
