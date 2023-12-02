04b688ad70e94c773d5d29ba5b18275dc314e496
oruccakir <oruccakir2525@gmail.com>
2023-12-02 22:47:28 +0300

    arraydeque is tested

--- DequeImplementation/test.c
+++ DequeImplementation/test.c
@@ -6,15 +6,18 @@ int main(){
     ArrayDeque dq;
     ArrayDeque *dq_ptr = &dq;
     initialize(dq_ptr);
-    addFirst(dq_ptr,12);
-    addFirst(dq_ptr,1);
-    addLast(dq_ptr,55);
-    addFirst(dq_ptr,21);
+    int a = 10, b = 20, c = 30;
+    addFirst(dq_ptr,a);
+    addFirst(dq_ptr,b);
+    addLast(dq_ptr,c);
+    addFirst(dq_ptr,a);
     
+    /*
     printf("%d\n\n",pollFirst(dq_ptr));
 
     printf("%d\n\n",pollLast(dq_ptr));
-
+    */
     printDeque(dq_ptr);
     printDequeReverse(dq_ptr);
+    
 }
